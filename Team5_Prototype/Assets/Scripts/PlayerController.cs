1968eb0094d10c618ddab18951240d44ea97e7a9
km16f <km16f@my.fsu.edu>
2018-10-21 16:49:16 -0400

    Added player movement and jumping

---
+++ Team5_Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D control;
+    public float jumpforce;
+    bool ground;
+
+    void Start()
+    {
+        control = GetComponent<Rigidbody2D>();
+        speed = 5;
+    }
+
+    
+
+    void FixedUpdate()
+    {
+        float sideMovement = Input.GetAxis("Horizontal");
+        control.velocity = new Vector2(sideMovement * speed, control.velocity.y);
+        Jump();
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && ground)
+        {
+            ground = false;
+            control.AddForce(new Vector2(control.velocity.x, jumpforce));
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            ground = true;
+            
+        }
+    }
+}
