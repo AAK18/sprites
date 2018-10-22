533b32e7a6b2c6e02819dd9d973f8b0c8837d68f
km16f <km16f@my.fsu.edu>
2018-10-22 00:23:53 -0400

    Added characters ability to pick up surgical tool and carry it with them

---
+++ Team5_Prototype/Assets/Scripts/PickupItem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour {
+
+    public GameObject player;
+    bool collected = false;
+    float xs, ys;
+	
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");	
+	}
+
+    void Update()
+    {
+        // xs = player.transform.position.x + 0.277;
+        //ys = player.transform.position.y + 0.062;
+        if (collected == true)
+        {
+
+            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
+            if (player.transform.localScale.x > 0)
+            {
+                Vector3 temp = gameObject.transform.position;
+                temp.x = player.transform.position.x + 0.277f;
+                temp.y = player.transform.position.y - 0.068f;
+                gameObject.transform.position = temp;
+
+                if (gameObject.transform.localScale.x > 0)
+                {
+                    Vector2 temp2 = gameObject.transform.localScale;
+                    temp2.x *= -1;
+                    gameObject.transform.localScale = temp2;
+                }
+            }
+            else if (player.transform.localScale.x < 0)
+            {
+                Vector3 temp = gameObject.transform.position;
+                temp.x = player.transform.position.x - 0.343f;
+                temp.y = player.transform.position.y - 0.068f;
+                gameObject.transform.position = temp;
+
+                if (gameObject.transform.localScale.x < 0)
+                {
+                    Vector2 temp2 = gameObject.transform.localScale;
+                    temp2.x *= -1;
+                    gameObject.transform.localScale = temp2;
+                }
+
+            }
+        }
+    }
+   
+    void OnCollisionEnter2D(Collision2D other)
+    {
+         if (other.gameObject.CompareTag("Player"))
+         {
+                gameObject.transform.position = other.transform.position;
+                collected = true;
+        }
+    }
+    
+}
