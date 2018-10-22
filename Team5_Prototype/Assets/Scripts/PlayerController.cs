4b89b9d16e47512e4553234f39fdf174bb792e90
km16f <km16f@my.fsu.edu>
2018-10-22 19:06:36 -0400

    Added basic walking animation

--- Team5_Prototype/Assets/Scripts/PlayerController.cs
+++ Team5_Prototype/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    public float speed, sideMovement = 0.0f;
     private Rigidbody2D control;
     public float jumpforce;
     bool ground;
     bool faceRight = true;
+    public Animator animate;
 
     void Start()
     {
         control = GetComponent<Rigidbody2D>();
+        speed = 5;
         
     }
 
@@ -22,19 +24,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
         Movement();
+       
     }
 
     void Movement()
     {
-        float sideMovement = Input.GetAxis("Horizontal");
-        control.velocity = new Vector2(sideMovement * speed, control.velocity.y);
+        sideMovement = Input.GetAxis("Horizontal") * speed;
+        if (sideMovement == 0) { animate.SetBool("KeyDown", false); }
+        else { animate.SetBool("KeyDown", true); }
+
+        control.velocity = new Vector2(sideMovement, control.velocity.y);
         
             Jump();
-        
+        animate.SetFloat("Speed", Mathf.Abs(sideMovement));
 
-        if(sideMovement < 0 && faceRight == true)
+        if (sideMovement < 0 && faceRight == true)
         {
             flip();
         }
